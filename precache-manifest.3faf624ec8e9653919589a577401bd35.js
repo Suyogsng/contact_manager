7645cb952375d9f0ada48ad3530c037040b3e83b
Suyog <suyogwaitforitsng@gmail.com>
2018-11-30 12:27:15 +0545

    Updates

---
+++ precache-manifest.3faf624ec8e9653919589a577401bd35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae20d9874e5c532fac44",
+    "url": "/contact_manager/static/css/main.b83989e3.chunk.css"
+  },
+  {
+    "revision": "ae20d9874e5c532fac44",
+    "url": "/contact_manager/static/js/main.ae20d987.chunk.js"
+  },
+  {
+    "revision": "d6342032ef2d38110cec",
+    "url": "/contact_manager/static/css/1.f497eb59.chunk.css"
+  },
+  {
+    "revision": "d6342032ef2d38110cec",
+    "url": "/contact_manager/static/js/1.d6342032.chunk.js"
+  },
+  {
+    "revision": "635860fd533afff61498",
+    "url": "/contact_manager/static/js/runtime~main.635860fd.js"
+  },
+  {
+    "revision": "32a2b6ed8e405a3d45e80b6701380846",
+    "url": "/contact_manager/index.html"
+  }
+];
